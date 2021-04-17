63b2baa14e5dafde9e9161b5718089a8f603a275
XinyueDang <sinreydang@gmail.com>
2021-04-17 17:45:09 -0400

    heroku deploy

--- server.js
+++ server.js
@@ -11,14 +11,15 @@ app.use(session({
     saveUninitialized: true,
 }))
 
+const MONGODB_URL = process.env.MONGODB_URL;
 const mongoose = require('mongoose');
 mongoose.connect(
-    'mongodb://localhost:27017/whiteboard',
+    MONGODB_URL,
     {useNewUrlParser: true, useUnifiedTopology: true});
 
 
 app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers',
         'Content-Type, X-Requested-With, Origin');
     res.header('Access-Control-Allow-Methods',
