b3ce035a7bc885984e8b82918f4f5a5973549f16
XinyueDang <sinreydang@gmail.com>
2021-04-17 15:03:35 -0400

    Create the model-related files

--- server.js
+++ server.js
@@ -1,13 +1,24 @@
-var express = require('express');
+var express = require("express");
 var app = express();
-app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers',
-        'Content-Type, X-Requested-With, Origin');
-    res.header('Access-Control-Allow-Methods',
-        'GET, POST, PUT, PATCH, DELETE, OPTIONS');
-    next();
+
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost:27017/whiteboard", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
-require('./controllers/quizzes-controller')(app);
-require('./controllers/question-controller')(app);
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Content-Type, X-Requested-With, Origin"
+  );
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, PATCH, DELETE, OPTIONS"
+  );
+  next();
+});
+require("./controllers/quizzes-controller")(app);
+require("./controllers/question-controller")(app);
 app.listen(process.env.PORT || 3001);
